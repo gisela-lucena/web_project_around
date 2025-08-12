6336f118627dfa4fc3fef96a851791b2da0849a8
Gisela Lucena <lucena.gisela@gmail.com>
2025-08-11 20:29:12 -0400

    feat: dev branch

---
+++ index.js
@@ -0,0 +1,26 @@
+const profileEditButton = document.querySelector('.profile__edit-button');
+const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close');
+let popupForm = document.querySelector('.popup__form');
+const popupInputEditName = document.querySelector('.popup__input_edit_name');
+const popupInputEditProfession = document.querySelector('.popup__input_edit_profession');
+const profileTitle = document.querySelector('.profile__title');
+const profileProfession = document.querySelector('.profile__subtitle');
+
+profileEditButton.addEventListener('click', () => {
+  popup.style.display = 'flex';
+}
+);
+
+popupCloseButton.addEventListener('click', () => {
+  popup.style.display = 'none';
+}
+);
+
+popupForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  profileTitle.textContent = popupInputEditName.value;
+  profileProfession.textContent = popupInputEditProfession.value;
+  popup.style.display = 'none';
+  }
+);
